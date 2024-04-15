1c3eb2175a99f81e389fa50bf6910315a8f9d445
RabbiPND <130862563+RabbiPND@users.noreply.github.com>
2024-04-15 18:38:08 +0200

    UPDATED QURIES

--- SQL_Analysis.sql
+++ SQL_Analysis.sql
@@ -20,12 +20,9 @@ LEFT JOIN dbo.meal_cost AS c
 ON h.meal = c.meal
 
 /*
-QUESTIONS
-
-1.Is the hotel revenue growing?, break down by hotel type
+ANALYSIS
 */
 
---1.
 SELECT *
 FROM #Hotels
 
@@ -41,6 +38,42 @@ GROUP BY
     h.arrival_date_year, h.hotel
 ORDER BY 
     h.arrival_date_year;
+
+SELECT ROUND(AVG(adr),2) AS "Average adr"
+FROM #Hotels
+
+SELECT ROUND(AVG(m.Discount),2) AS "Average discount"
+FROM #Hotels AS h
+LEFT JOIN 
+dbo.market_segment AS m ON h.market_segment = m.market_segment
+
+SELECT SUM(stays_in_week_nights + stays_in_weekend_nights) AS "Total nights"
+FROM #Hotels
+
+
+------------------------------------------------------------------------
+--STANDARDIZED DATE
+
+SELECT CONVERT(date, reservation_status_date) AS standardized_reservation_status_date
+FROM #Hotels
+
+ALTER TABLE #Hotels
+ADD standardized_reservation_status_date DATE;
+
+UPDATE #Hotels
+SET standardized_reservation_status_date = CONVERT(DATE, reservation_status_date, 126)
+
+
+SELECT reservation_status_date, standardized_reservation_status_date
+FROM #Hotels;
+
+
+SELECT h.standardized_reservation_status_date, ROUND(SUM((h.stays_in_week_nights + h.stays_in_weekend_nights) * (h.adr - (h.adr * m.Discount))), 2) AS "Total Revenue"
+FROM #Hotels AS h
+LEFT JOIN 
+dbo.market_segment AS m ON h.market_segment = m.market_segment
+GROUP BY h.standardized_reservation_status_date
+
 
 
 
